a6979e85c773a48c24048e5c8ef1577138d4b707
massive001 <a.asodiq1606@gmail.com>
2022-09-19 15:59:03 -0700

    Parameter Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to the parameter and updates the parameter value.
+ * @n: input integer.
+ * Return: no return.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
