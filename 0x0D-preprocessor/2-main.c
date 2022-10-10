be7ea85a6891b75317e0fd8e6d262b19a038aaec
massive001 <a.asodiq1606@gmail.com>
2022-10-10 16:56:02 -0700

    tassskk Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*** main (0) -> print the file name 
+ * return -> void 
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+This task is on further knowledge of preprocessor 
+all files were programmed on git bash 
+tools used is vim editor 
